030b62f7693316b5340c1d029c5bb75045bba4ac
msaeed80 <eng.mohamed.said80@gmail.com>
2019-03-02 19:32:56 +0300

    Add project files.

---
+++ HashCodeOnline/InputReader.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeOnline
+{
+    class InputReader
+    {
+        private string fileName;
+
+        public InputReader(string fname)
+        {
+            this.fileName = fname;
+        }
+
+        public Problem ReadInput()
+        {
+            Problem p = new Problem();
+
+            p.slides = new List<Slide>();
+
+            string[] AllLines = File.ReadAllLines(fileName);
+
+            int lineIndex = 0;
+            //setup line
+            string[] line = AllLines[lineIndex].Split(' ');
+
+            p.N = int.Parse(line[0]);
+
+            p.photos = new Photo[p.N];
+
+            p.tags = new List<Tag>();
+
+            Hashtable hashtable = new Hashtable();
+
+            
+
+            //EndPoints
+            for (int e = 0; e < p.N; e++)
+            {
+                lineIndex++;
+                line = AllLines[lineIndex].Split(' ');
+
+                
+
+                p.photos[e] = new Photo();
+
+                p.photos[e].i = e;
+
+                p.photos[e].taken = false;
+
+                p.photos[e].H_V = line[0];
+
+                p.photos[e].M = int.Parse(line[1]);
+
+                p.photos[e].tags = new List<string>();
+
+                int t = 2;
+                //Photos
+                for (int c = 0; c < p.photos[e].M; c++)
+                {
+                    var currTag = line[t++];
+
+                    p.photos[e].tags.Add(currTag);
+
+                    bool found = hashtable.ContainsKey(currTag);
+                    //var tag = p.tags.Find(tg => tg.tagString == currTag);
+
+                    if(!found)
+                    {
+                        //add tag
+                        Tag tt = new Tag
+                        {
+                            tagString = currTag,
+                            pc = 1,
+                            photos = new List<int>(),
+
+                        };
+
+                        tt.photos.Add(e);
+
+                        hashtable.Add(currTag, tt);
+
+                        //p.tags.Add(tt);
+                    }
+                    else
+                    {
+                        var tt = hashtable[currTag] as Tag;
+                        
+                        tt.pc++;
+                        tt.photos.Add(e);
+
+                        hashtable[currTag] = tt;
+                    }
+                }
+            }
+
+            foreach(DictionaryEntry d in hashtable)
+            {
+                p.tags.Add(d.Value as Tag);
+            }
+
+            Console.WriteLine(fileName + " Read Complete");
+
+            return p;
+        }
+    }
+}
---
+++ SourceCode/Problem.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeOnline
+{
+    class Problem
+    {
+        public int N;
+       
+
+        public Photo[] photos;
+
+        public List<Tag> tags;
+
+        public List<Slide> slides;
+
+        public Problem()
+        {
+            
+        }
+
+        public void Solve(string outFileName)
+        {
+            //CreateSlidesByTags();
+            CreateSlidesByPhoto();
+            Output(outFileName);
+        }
+
+        private void CreateSlidesByTags()
+        {
+            var SortedTags = tags.OrderByDescending(t => t.pc);
+            Photo vert1 = null, vert2 = null;
+
+            foreach (Tag t in SortedTags)
+            {
+                
+
+                foreach(int p in t.photos)
+                {
+                    var ph = photos[p];
+
+                    
+
+                    if(ph.H_V == "H" && !ph.taken)
+                    {
+                        ph.taken = true;
+
+                        var slide = new Slide();
+
+                        slide.photos.Add(p);
+
+                        slides.Add(slide);
+                    }
+                    else
+                    {
+                        if (vert1 == null && !ph.taken)
+                        {
+                            vert1 = ph;
+                            vert1.taken = true;
+                        }
+                        else if (vert2 == null && !ph.taken)
+                        {
+                            vert2 = ph;
+
+                            var slide = new Slide();
+
+                            slide.photos.Add(vert1.i);
+                            slide.photos.Add(vert2.i);
+
+                            slides.Add(slide);
+
+                            vert2.taken = true;
+
+                            vert1 = null;
+                            vert2 = null;
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        private void CreateSlidesByPhoto()
+        {
+            List<Photo> photosList;
+
+            Photo vert1 = null, vert2 = null;
+
+
+            foreach (Photo p in photos)
+            {
+                var orderedTags = p.tags.OrderBy(t => t);
+
+                foreach (string t in orderedTags)
+                {
+                    p.concatTags += t + " ";
+                }
+            }
+
+            photosList = photos.ToList();
+
+            var orderedPhotos = photosList.OrderBy(p => p.concatTags).ThenByDescending(p => p.concatTags.Length);
+
+            foreach(Photo ph in orderedPhotos)
+            {
+                if (ph.H_V == "H" && !ph.taken)
+                {
+                    ph.taken = true;
+
+                    var slide = new Slide();
+
+                    slide.photos.Add(ph.i);
+
+                    slides.Add(slide);
+                }
+                else
+                {
+                    if (vert1 == null && !ph.taken)
+                    {
+                        vert1 = ph;
+                        vert1.taken = true;
+                    }
+                    else if (vert2 == null && !ph.taken)
+                    {
+                        vert2 = ph;
+
+                        var slide = new Slide();
+
+                        slide.photos.Add(vert1.i);
+                        slide.photos.Add(vert2.i);
+
+                        slides.Add(slide);
+
+                        vert2.taken = true;
+
+                        vert1 = null;
+                        vert2 = null;
+                    }
+                }
+                
+
+            }
+
+
+        }
+
+        private void Output(string outFileName)
+        {
+            File.AppendAllText(outFileName, slides.Count() + Environment.NewLine);
+
+            //Console.WriteLine(slides.Count());
+
+            string photoOut = "";
+
+            foreach (Slide s in slides)
+            {
+                
+                foreach(int p in s.photos)
+                {
+
+                    photoOut += p + " ";
+                }
+
+                File.AppendAllText(outFileName, photoOut + Environment.NewLine);
+                //Console.WriteLine(photoOut);
+
+                photoOut = "";
+            }
+        }
+    }
+
+    class Slide
+    {
+        public Slide()
+        {
+            photos = new List<int>();
+        }
+
+        public List<int> photos;
+    }
+
+    class Tag
+    {
+        public string tagString;
+        public int pc;
+        public List<int> photos;
+    }
+    class Photo
+    {
+        public int i;
+        public bool taken;
+        public string H_V;
+        public int M;
+        public List<string> tags;
+        public string concatTags;    
+    }
+
+
+
+}
